8b4d95ecdeeb1d8dde63ba931dbb0c51f47eb9a1
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-07-12 00:49:56 -0600

    Making a branch after this commit. added soundcheck logo to webbrowser icon. also added auth component. ( we can change the comp name )

---
+++ src/Components/AuthPage/Auth.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react"
+import styled from "styled-components"
+
+export default class Auth extends Component {
+  render() {
+    return <Container>container</Container>
+  }
+}
