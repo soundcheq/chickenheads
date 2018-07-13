13a7fdf8d28df75f062e01489f0ad4760508e5ee
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-07-12 19:20:10 -0600

    added basic layout of the user panel ( right side ) left an area for when we map over events to display.

--- src/Components/RightPanel/RightPanel.js
+++ src/Components/RightPanel/RightPanel.js
@@ -12,17 +12,21 @@ export default class componentName extends Component {
         <UserContainer>
           <UserIcon />
           <InfoContainer>
-            <Welcome>welcome back,</Welcome>
+            <Welcome>Welcome back,</Welcome>
             <UserCont>
               <User>{this.state.user}</User>
               <UserButton>^</UserButton>
             </UserCont>
+            <LINE />
             <PointsContainer>
               <Points>{this.state.points}</Points>
               <div>ICON</div>
             </PointsContainer>
           </InfoContainer>
         </UserContainer>
+        {/* TEMPORARY UNTIL DATA ADDED */}
+        <div style={{ height: "81.4vh" }}>MAP EVENTS HERE.</div>
+        {/* TEMPORARY UNTIL DATA ADDED */}
       </RightPanelContainer>
     )
   }
@@ -43,6 +47,7 @@ const UserContainer = styled.div`
   color: white;
   justify-content: space-around;
   padding: 2rem 1rem;
+  letter-spacing: 1px;
 `
 const UserIcon = styled.div`
   height: 72px;
@@ -54,12 +59,13 @@ const InfoContainer = styled.div`
   width: 58%;
 `
 const Welcome = styled.div`
-  font-size: 13px;
+  font-size: 11px;
+  opacity: 0.6;
 `
 const User = styled.div`
   font-size: 18px;
-  border-bottom: 1px solid white;
   margin: 0.5rem 0;
+  margin-bottom: 2px;
   padding-bottom: 0.25rem;
 `
 const Points = styled.div`
@@ -68,7 +74,9 @@ const Points = styled.div`
   padding: 4px;
   font-size: 14px;
   width: 32px;
-  background: rgb(0, 0, 0, 0.25);
+  background: rgb(0, 0, 1, 0.25);
+  margin-right: 0.5rem;
+  border-radius: 4px;
 `
 const PointsContainer = styled.section`
   ${f};
@@ -77,11 +85,24 @@ const PointsContainer = styled.section`
 
 const UserCont = styled.section`
   ${f};
+  align-items: center;
 `
 const UserButton = styled.button`
-  width: 25px;
-  height: 25px;
-  background: rgb(0, 0, 0, 0.25);
+  padding: 0;
+  border: none;
+  background: none;
+  width: 28px;
+  height: 20px;
+  background: rgb(0, 0, 0, 0.2);
   margin-left: 1rem;
   outline: transparent;
+  border-radius: 4px;
+  color: white;
+`
+
+const LINE = styled.div`
+  border-top: 1px solid white;
+  width: 8rem;
+  height: 1px;
+  margin-bottom: 0.25rem;
 `
