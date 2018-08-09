720076d32b487170aa9dc20240496beec3740f1a
James Lemire <webdev.jameslemire@gmail.com>
2018-08-09 17:31:30 -0600

    JL master - Login Venue Contact with code comments.

--- middlewares/login_middleware.js
+++ middlewares/login_middleware.js
@@ -1,1 +1,17 @@
-module.exports = {}
+module.exports = {
+  sanitizeVenueContact: (req, res, next) => {
+    const { email, password } = req.body
+
+    if (typeof email !== 'string')
+      return res
+        .status(409)
+        .send('Request body missing required email property (Type: string)')
+
+    if (typeof password !== 'string')
+      return res
+        .status(409)
+        .send('Request body missing required password property (Type: string)')
+
+    next()
+  }
+}
--- router/login_router.js
+++ router/login_router.js
@@ -1,6 +1,11 @@
 const Router = require('express').Router()
 const login_controller = require('../controllers').login_controller
+const login_middleware = require('../middlewares').login_middleware
 
-Router.post('/', login_controller.login)
+Router.post(
+  '/venue',
+  login_middleware.sanitizeVenueContact,
+  login_controller.login_venue_contact
+)
 
 module.exports = Router
