81fbb11a50e5ec2ea58064cadc84d80caa3fa46a
James Lemire <webdev.jameslemire@gmail.com>
2018-08-09 17:17:02 -0600

    JL master - Add more code comments to logic.

--- controllers/register_controller.js
+++ controllers/register_controller.js
@@ -22,13 +22,16 @@ module.exports = {
       // Delete unnecessary value
       delete req.body.confirm_password
 
+      // Check for an existing contact by email address and phone
       const existing_contact = await VenueContacts.findOne({
         where: { [Op.or]: [{ email }, { phone }] }
       })
 
+      // A contact already exists with the provided Email or Phone
       if (existing_contact)
         return res.status(500).send('Email or Phone Number is already taken')
 
+      // This code only runs if there is not an existing_contact
       // Salt and hash the password
       const salt = await bcrypt.genSalt(saltRounds)
       const hashed_password = await bcrypt.hash(password, salt)
@@ -36,7 +39,9 @@ module.exports = {
       // Update the password on the request body to be the hashed version
       req.body.password = hashed_password
 
+      // Create the contact using the values from the request body
       let venue_contact = await VenueContacts.create(req.body)
+      // Delete the password from the contact before sending it back to the client
       delete venue_contact.dataValues.password
 
       res.send(venue_contact)
