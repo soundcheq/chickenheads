f6e03fa7fc6df2e39ea64e3c6fb30ba6b1aeaf59
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-08-21 10:27:40 -0600

    .

--- public/src/PageViews/UserRegister/UserRegister/RegisterUserForm.js
+++ public/src/PageViews/UserRegister/UserRegister/RegisterUserForm.js
@@ -3,6 +3,26 @@ import styled from 'styled-components'
 
 export default class RegisterForm extends Component {
   render() {
-    return <div>Edit Your Profile</div>
+    return (
+      <div>
+        <div>Edit Your Profile</div>
+        <button>Go Back</button>
+        <section>
+          <input type="text" placeholder="Name" name="" />
+          <input type="text" placeholder="Email" name="" />
+          <input type="text" placeholder="Birthday" name="" />
+          <input type="text" placeholder="Location" name="" />
+          <input type="text" placeholder="" name="" />
+          <input type="text" placeholder="" name="" />
+          <input type="text" placeholder="" name="" />
+        </section>
+      </div>
+    )
   }
 }
+
+const FormContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
