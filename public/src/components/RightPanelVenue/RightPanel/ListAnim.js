f6793bafa6949e6d573bc055fe7b73890f4a4391
Tav Hafner <tav.hafner@gmail.com>
2018-09-03 13:29:31 -0600

    basic list reordering animations

--- public/src/components/RightPanelVenue/RightPanel/ListAnim.js
+++ public/src/components/RightPanelVenue/RightPanel/ListAnim.js
@@ -3,9 +3,17 @@ import styled from 'styled-components'
 
 import { Flipper, Flipped } from 'react-flip-toolkit'
 
+const items = [
+  { id: 1, color: 'green' },
+  { id: 2, color: 'red' },
+  { id: 3, color: 'blue' },
+  { id: 4, color: 'yellow' }
+]
+
 export default class ListAnim extends Component {
   state = {
-    toggle: false
+    toggle: false,
+    order: items.map(item => item.id)
   }
 
   toggleDiv = () => {
@@ -14,17 +22,26 @@ export default class ListAnim extends Component {
     }))
   }
 
+  changeOrder = () => {
+    let newOrder = this.state.order;
+    newOrder = [...newOrder.splice(newOrder.length - 1, 1), ...newOrder]
+    this.setState({
+      order: newOrder
+    })
+  }
+
   render() {
     return (
-      <Flipper flipKey={this.state.toggle}>
-        <Wrapper>
-          <Flipped flipId="square">
-            {this.state.toggle ? (
-              <OtherDiv onClick={this.toggleDiv} />
-            ) : (
-              <TestDiv onClick={this.toggleDiv} />
-            )}
-          </Flipped>
+      <Flipper flipKey={this.state.order}>
+        <Wrapper onClick={this.changeOrder}>
+          {
+            this.state.order.map(id => items.filter(item => item.id === id)[0])
+              .map((item, idx) => {
+                return <Flipped flipId={`item-${item.id}`}>
+                  <TestDiv background={item.color}/>
+                </Flipped>
+              })
+          }
         </Wrapper>
       </Flipper>
     )
@@ -43,7 +60,7 @@ const Wrapper = styled.div`
 const TestDiv = styled.div`
   width: 80%;
   height: 30px;
-  background-color: red;
+  background-color: ${props => props.background};
   margin: 5px auto;
 `
 
