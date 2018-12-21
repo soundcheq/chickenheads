6e51b745ad5790175b4b106c474f80c28acc0ff8
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-12-21 13:51:37 -0600

    initial commit with a few styled components.

--- public/src/PageViews/Dashboard/Dashboard.js
+++ public/src/PageViews/Dashboard/Dashboard.js
@@ -5,13 +5,36 @@ export default class Dashboard extends Component {
   render() {
     return (
       <Container>
-        <span>I am Dashboard</span>
+        <Card>
+          I Am Micro Stats.
+        </Card>
+
+        <Card height="60vh">
+          I Am Macro Stats.
+        </Card>
       </Container>
     )
   }
 }
+
+
 const f = 'display: flex'
 
 const Container = styled.section`
   ${f};
+  width: 100%;
+  height: 100vh;
+  background-color: #e8e8e8;
+  flex-direction: column;
+  justify-content: space-evenly;
+  
 `
+const Card = styled.div`
+  ${f}
+  background-color: white;
+  border-radius: 5px;
+  width: 96%;
+  height: ${props => props.height ? props.height : '30vh'}
+  margin: 0% 2%;
+`
+
